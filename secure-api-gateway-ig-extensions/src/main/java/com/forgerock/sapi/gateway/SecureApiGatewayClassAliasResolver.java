ca6790b5349c3f9080e62b09178db4c8150487e2
Wayne Morrison <wayne.morrison@forgerock.com>
2025-05-21 16:07:21 +0100

    OPENIG-9136 OPENIG-9340 Reintroduce roles-based scope validation - Reintroduce validator as filter RegistrationRequestRoleBasedScopeValidationFilter - Prevent removal of protection for ITSS - Previously part or ProcessRegistration script - This should really reside in OBUK AS, but reinstating this in SAPIG-core as we don't currently have that env (and can't lose the protection).

--- secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
+++ secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
@@ -23,14 +23,17 @@ import org.forgerock.openig.alias.ClassAliasResolver;
 import com.forgerock.sapi.gateway.am.AccessTokenResponseIdTokenReSignFilter;
 import com.forgerock.sapi.gateway.am.AuthorizeResponseJwtReSignFilter;
 import com.forgerock.sapi.gateway.am.JwtReSigner;
+import com.forgerock.sapi.gateway.dcr.filter.RegistrationRequestRoleBasedScopeValidationFilter;
 
 public class SecureApiGatewayClassAliasResolver implements ClassAliasResolver {
     private static final Map<String, Class<?>> ALIASES = new HashMap<>();
 
     static {
-        ALIASES.put("JwtReSigner", JwtReSigner.class);
         ALIASES.put("AccessTokenResponseIdTokenReSignFilter", AccessTokenResponseIdTokenReSignFilter.class);
         ALIASES.put("AuthorizeResponseJwtReSignFilter", AuthorizeResponseJwtReSignFilter.class);
+        ALIASES.put("JwtReSigner", JwtReSigner.class);
+        ALIASES.put("RegistrationRequestRoleBasedScopeValidationFilter",
+                    RegistrationRequestRoleBasedScopeValidationFilter.Heaplet.class);
     }
 
     /**
