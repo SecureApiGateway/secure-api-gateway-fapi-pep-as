01f769f40e73bc35becf6de28ebb157f90f03a4e
Wayne Morrison <wayne.morrison@forgerock.com>
2025-07-08 13:36:05 +0100

    DO-NOT-MERGE: Version with alternative fixed consentIdLocator provided with an expression (but had to escape the "${}" value in the test).

--- config/7.3.0/fapi1part2adv/ig/scripts/groovy/AuditConsentOB.groovy
+++ config/7.3.0/fapi1part2adv/ig/scripts/groovy/AuditConsentOB.groovy
@@ -1,5 +1,13 @@
-import static org.forgerock.json.resource.Requests.newCreateRequest;
-import static org.forgerock.json.resource.ResourcePath.resourcePath;
+import static org.forgerock.json.resource.Requests.newCreateRequest
+import static org.forgerock.json.resource.ResourcePath.resourcePath
+import static org.forgerock.openig.el.Bindings.bindings
+import static org.forgerock.util.promise.NeverThrowsException.neverThrown
+
+import org.forgerock.json.resource.ResourceException
+import org.forgerock.json.resource.ResourceResponse
+import org.forgerock.openig.el.Bindings
+import org.forgerock.openig.el.Expression
+
 //TODO: This is no longer available on this import
 //import static com.forgerock.sapi.gateway.rest.HttpHeaderNames.X_FAPI_INTERACTION_ID
 X_FAPI_INTERACTION_ID = "x-fapi-interaction-id";
@@ -28,27 +36,36 @@ def resourceEvent() {
 }
 
 
-next.handle(context, request).thenOnResult(response -> {
-    logger.debug(SCRIPT_NAME + "Running...")
-    if (!response.status.isSuccessful()) {
-        logger.info(SCRIPT_NAME + "Error response, skipping audit")
-        return
-    }
-    // NO_CONTENT is typically returned by deletes, these are currently not supported as the consentIdLocator will fail
-    if (response.status == Status.NO_CONTENT) {
-        logger.info(SCRIPT_NAME + "No Content response, skipping audit")
-        return
-    }
+next.handle(context, request)
+    .thenAsync(response -> {
+        logger.debug(SCRIPT_NAME + "Running...")
+        if (!response.status.isSuccessful()) {
+            logger.info(SCRIPT_NAME + "Error response, skipping audit")
+            return
+        }
+        // NO_CONTENT is typically returned by deletes, these are currently not supported as the consentIdLocator will fail
+        if (response.status == Status.NO_CONTENT) {
+            logger.info(SCRIPT_NAME + "No Content response, skipping audit")
+            return
+        }
 
-    logger.info(SCRIPT_NAME + context)
+        logger.info(SCRIPT_NAME + context)
 
-    def binding = new Binding()
-    binding.response = response
-    binding.contexts = contexts
-    logger.info(SCRIPT_NAME + "consentIdLocator={}", consentIdLocator)
-    consentId = new GroovyShell(binding).evaluate(consentIdLocator)
-    logger.info(SCRIPT_NAME + "consentId={} ({})", consentId, consentId.getClass())
+        logger.info(SCRIPT_NAME + "consentIdLocatorExpr={}", consentIdLocatorExpr)
+        Bindings bindings = bindings(context).bind("response", response)
+        Expression<String> consentIdExpr = Expression.valueOf(consentIdLocatorExpr, String.class, bindings)
+        return consentIdExpr.evalAsync(bindings)
+                            .then(consentId -> {
+                                if (consentId == null) {
+                                    throw new IllegalStateException(
+                                            "Failed to find consentId as 'consentIdLocatorExpr' evaluated to null");
+                                }
+                                return fireAudit(consentId)
+                            }, neverThrown())
+                            .then(ignore -> response)
+})
 
+Promise<ResourceResponse, ResourceException> fireAudit(String consentId) {
     def consent = object(
             field('id', consentId),
             field('role', role)
@@ -65,6 +82,6 @@ next.handle(context, request).thenOnResult(response -> {
     auditEvent = auditEvent.add('fapiInfo', fapiInfo);
 
     // Send the event
-    auditService.handleCreate(context, auditEventRequest("ObConsentTopic", auditEvent))
     logger.debug(SCRIPT_NAME + "audited event for consentId: " + consentId)
-})
+    return auditService.handleCreate(context, auditEventRequest("ObConsentTopic", auditEvent))
+}
--- secure-api-gateway-ig-extensions/src/test/java/com/forgerock/sapi/gateway/scripts/AuditConsentObTest.java
+++ secure-api-gateway-ig-extensions/src/test/java/com/forgerock/sapi/gateway/scripts/AuditConsentObTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2020-2025 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.forgerock.sapi.gateway.scripts;
 
 import static java.util.Arrays.asList;
@@ -160,8 +175,9 @@ public class AuditConsentObTest extends AbstractScriptTest {
                            field("args",
                                  object(field("auditService", "${heap['AuditService']}"),
                                         field("clock", "${heap['Clock']}"),
-                                        // TODO[use-me-to-break-the-script-again] field("consentIdLocator", "contexts.attributes.openbanking_intent_id"),
-                                        field("consentIdLocator", "contexts.attributes.attributes.openbanking_intent_id"),
+                                        field("consentIdLocatorExpr", "\\${attributes.openbanking_intent_id}"),
+                                        field("consentIdLocator", "contexts.attributes.openbanking_intent_id"),
+                                        // TODO[fix/break "consentIdLocator"] field("consentIdLocator", "contexts.attributes.attributes.openbanking_intent_id"),
                                         field("role", "CBPII"),
                                         field("event", "EXEC")))));
     }
