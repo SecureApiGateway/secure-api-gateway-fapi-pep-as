7b81d5980bdb4609d91f232d9fdacaff6fe038ce
Wayne Morrison <wayne.morrison@forgerock.com>
2025-08-29 12:12:54 +0100

    OPENIG-9436 JwtReconstruction octet-sequence signed JWT support.

--- secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/jwt/OctetSequenceSignedJwt.java
+++ secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/jwt/OctetSequenceSignedJwt.java
@@ -18,53 +18,57 @@ package com.forgerock.sapi.gateway.jwt;
 import org.forgerock.json.jose.jws.JwsHeader;
 import org.forgerock.json.jose.jws.SignedJwt;
 import org.forgerock.json.jose.jws.handlers.SigningHandler;
-import org.forgerock.json.jose.jwt.Payload;
 
 /**
- * Extension of {@link SignedJwt} supporting plain {@link String} nested payload.
+ * Extension of {@link SignedJwt} supporting octet-sequence payloads represented by a {@link OctetSequencePayload}.
  */
-public class SignedJwtWithStringPayload extends SignedJwt {
+public class OctetSequenceSignedJwt extends SignedJwt {
 
     /**
-     * Construct a {code StringBasedSignedJwt} from an existent one.
+     * Construct an {@code OctetSequenceSignedJwt} from an existent one.
      * @param signedJwt the signed JWT
      */
-    protected SignedJwtWithStringPayload(final SignedJwtWithStringPayload signedJwt) {
+    protected OctetSequenceSignedJwt(final OctetSequenceSignedJwt signedJwt) {
         super(signedJwt);
     }
 
     /**
-     * Constructs a fresh, new SignedJwt from the given JwsHeader and nested Encrypted JWT.
+     * Constructs a fresh, new {@code OctetSequenceSignedJwt} from the given {}JwsHeader and nested Encrypted JWT.
      * <p>
      * The specified private key will be used in the creation of the JWS signature.
      *
      * @param header The JwsHeader containing the header parameters of the JWS.
-     * @param nestedPayload The nested payload that will be the payload of this JWS.
+     * @param nestedPayload The nested {@link OctetSequencePayload} that will be the payload of this JWS.
      * @param signingHandler The SigningHandler instance used to sign the JWS.
      */
-    public SignedJwtWithStringPayload(final JwsHeader header,
-                                      final Payload nestedPayload,
-                                      final SigningHandler signingHandler) {
+    public OctetSequenceSignedJwt(final JwsHeader header,
+                                  final OctetSequencePayload nestedPayload,
+                                  final SigningHandler signingHandler) {
         super(header, nestedPayload, signingHandler);
     }
 
     /**
-     * Constructs a reconstructed SignedJwt from its constituent parts, the JwsHeader, nested Encrypted JWT, signing
-     * input and signature.
+     * Constructs a reconstructed {@code OctetSequenceSignedJwt} from its constituent parts, the JwsHeader, nested
+     * Encrypted JWT, signing input and signature.
      * <p>
      * For use when a signed nested encrypted JWT has been reconstructed from its base64url encoded string
      * representation and the signature needs verifying.
      *
      * @param header The JwsHeader containing the header parameters of the JWS.
-     * @param nestedPayload The nested payload that is the payload of the JWS.
+     * @param nestedPayload The nested {@link OctetSequencePayload} that is the payload of the JWS.
      * @param signingInput The original data that was signed, being the base64url encoding of the JWS header and
      *                     payload concatenated using a "." character.
      * @param signature The resulting signature of signing the signing input.
      */
-    public SignedJwtWithStringPayload(final JwsHeader header,
-                                      final Payload nestedPayload,
-                                      final byte[] signingInput,
-                                      final byte[] signature) {
+    public OctetSequenceSignedJwt(final JwsHeader header,
+                                  final OctetSequencePayload nestedPayload,
+                                  final byte[] signingInput,
+                                  final byte[] signature) {
         super(header, nestedPayload, signingInput, signature);
+    }
+
+    @Override
+    public OctetSequencePayload getPayload() {
+        return (OctetSequencePayload) super.getPayload();
     }
 }
